1dcf7e4c4f729aa405c23da7605fc26d9f41e698
kirito <kirito666codr@gmail.com>
2026-04-18 15:15:38 +0530

    feat(auth): integrate MongoDB for user authentication and session management
    - Added MongoDB connection utility to handle database interactions.
    - Implemented user schema validation using Zod for user data during sign-in.
    - Enhanced sign-in callback to check for existing users and insert new users into the database.
    - Updated session callback to include user ID and role from the database in the session object.
    - Created TypeScript definitions for extended session and user types in NextAuth.

---
+++ src/models/user.schema.ts
@@ -0,0 +1,11 @@
+import z from 'zod';
+
+export const userSchema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  image: z.string().url().optional(),
+  role: z.enum(['user', 'admin']),
+  createdAt: z.date(),
+});
+
+export type User = z.infer<typeof userSchema>;
