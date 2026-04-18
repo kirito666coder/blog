e4f2be2737363326a257d919b6f3d760ce2a1eed
kirito <kirito666codr@gmail.com>
2026-04-18 17:57:53 +0530

    feat: implement Navbar component with conditional authentication sign-in and sign-out buttons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,28 @@
 import Link from 'next/link';
 import CutCornerButton from '../CutCornerButton';
+import { auth } from '@/auth';
 import SignIn from '../sign-in';
+import SignOut from '../sign-out';
 
-export function Navbar() {
+export async function Navbar() {
+  const session = await auth();
   return (
     <header className="sticky top-5 z-50 w-full">
-      <div className="mx-auto flex h-16 max-w-7xl items-center gap-10 px-6">
-        <Link
-          href="/"
-          className="font-ops text-xl tracking-tighter transition-opacity hover:opacity-80"
-        >
-          KIRITO.BLOG
-        </Link>
+      <div className="mx-auto flex h-16 max-w-7xl items-center justify-between gap-10 px-6">
+        <div className="flex items-center justify-center gap-10">
+          <Link
+            href="/"
+            className="font-ops text-xl tracking-tighter transition-opacity hover:opacity-80"
+          >
+            KIRITO.BLOG
+          </Link>
 
-        <nav className="flex items-center gap-8">
-          <CutCornerButton text="Blogs </>" url="/blogs" />
-          <CutCornerButton text="About </>" url="/about" />
-        </nav>
-        <SignIn />
+          <nav className="flex items-center gap-8">
+            <CutCornerButton text="Blogs </>" url="/blogs" />
+            <CutCornerButton text="About </>" url="/about" />
+          </nav>
+        </div>
+        {session ? <SignOut /> : <SignIn />}
       </div>
     </header>
   );
--- src/components/sign-out.tsx
+++ src/components/sign-out.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { signIn } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import { useRef } from 'react';
 import type { CSSProperties } from 'react';
 
-export default function SignIn() {
+export default function SignOut() {
   const btnRef = useRef<HTMLButtonElement | null>(null);
 
   const applyEnter = () => {
@@ -36,14 +36,14 @@ export default function SignIn() {
       onMouseLeave={() => {
         applyLeave();
       }}
-      className="cursor-pointer border-white py-1.5 hover:border"
+      className="cursor-pointer border-red-500 py-1.5 hover:border"
       style={{
         clipPath:
           'polygon(14px 0%, 100% 0%, 100% calc(100% - 14px), calc(100% - 14px) 100%, 0% 100%, 0% 14px)',
       }}
     >
       <button
-        onClick={() => signIn('github')}
+        onClick={() => signOut()}
         ref={btnRef}
         className="relative cursor-pointer overflow-hidden px-8 py-2 font-semibold tracking-wide text-white"
         style={
@@ -72,18 +72,10 @@ export default function SignIn() {
         <span className="shine blue" />
         <span className="shine red" />
 
-        <span className="relative z-10 flex items-center justify-between gap-2 py-px text-sm">
+        <span className="relative z-10 flex items-center justify-between gap-2 py-1 text-sm">
           {/* GitHub Icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="18"
-            height="18"
-            fill="currentColor"
-          >
-            <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.57.1.78-.25.78-.55 0-.27-.01-1.16-.02-2.1-3.2.7-3.88-1.38-3.88-1.38-.53-1.34-1.3-1.7-1.3-1.7-1.06-.72.08-.71.08-.71 1.18.08 1.8 1.21 1.8 1.21 1.04 1.79 2.73 1.27 3.39.97.1-.76.41-1.27.75-1.56-2.55-.29-5.24-1.27-5.24-5.66 0-1.25.45-2.27 1.2-3.07-.12-.3-.52-1.52.11-3.16 0 0 .97-.31 3.18 1.17a11.1 11.1 0 0 1 5.8 0c2.2-1.48 3.17-1.17 3.17-1.17.64 1.64.24 2.86.12 3.16.75.8 1.2 1.82 1.2 3.07 0 4.4-2.7 5.36-5.27 5.64.42.36.8 1.08.8 2.18 0 1.58-.01 2.86-.01 3.25 0 .3.2.66.79.55A11.51 11.51 0 0 0 23.5 12c0-6.35-5.15-11.5-11.5-11.5Z" />
-          </svg>
-          <span>GitHub</span>
+
+          <span>Sign Out</span>
         </span>
 
         <style jsx>{`
